5defccab84203718b25a27fe5bdb927cfade3a0d
Dhananjay <dhananjayjadhav2151@gmail.com>
2024-02-14 15:24:42 +0530

    State

--- 6. Lets get Hooked!/src/config.js
+++ 6. Lets get Hooked!/src/config.js
@@ -1,36 +1,33 @@
 export const IMG_CND_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e33e1d3ba7d6b2bb0d45e1001b731fcf";
 
-export const restaurantList = {
-  type: "restaurants",
-  cards: [
-    {
-      restId: "1",
-      name: "Burgar King",
-      image: IMG_CND_URL,
-      cuisines: ["Burgar", "American"],
-      rating: "4.2",
-    },
-    {
-      restId: "2",
-      name: "Parcel point",
-      image: IMG_CND_URL,
-      cuisines: ["Mexican", "Italian"],
-      rating: "3.2",
-    },
-    {
-      restId: "3",
-      name: "Koli wada",
-      image: IMG_CND_URL,
-      cuisines: ["Wada Pav", "Street Food"],
-      rating: "4.2",
-    },
-    {
-      restId: "4",
-      name: "Masala chat",
-      image: IMG_CND_URL,
-      cuisines: ["Chat", "Idli"],
-      rating: "2.2",
-    },
-  ],
-};
+export const restaurantList = [
+  {
+    restId: "1",
+    name: "Burgar King",
+    image: IMG_CND_URL,
+    cuisines: ["Burgar", "American"],
+    rating: "4.2",
+  },
+  {
+    restId: "2",
+    name: "Parcel point",
+    image: IMG_CND_URL,
+    cuisines: ["Mexican", "Italian"],
+    rating: "3.2",
+  },
+  {
+    restId: "3",
+    name: "Koli wada",
+    image: IMG_CND_URL,
+    cuisines: ["Wada Pav", "Street Food"],
+    rating: "4.2",
+  },
+  {
+    restId: "4",
+    name: "Masala chat",
+    image: IMG_CND_URL,
+    cuisines: ["Chat", "Idli"],
+    rating: "2.2",
+  },
+];
