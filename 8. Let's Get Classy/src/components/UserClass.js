76186c9a94f7ef02b357d27336c4a528676cee4b
d2j1 <dhananjayjadhav2151@gmail.com>
2025-09-13 18:18:54 +0530

    Class component completed

--- 8. Let's Get Classy/src/components/UserClass.js
+++ 8. Let's Get Classy/src/components/UserClass.js
@@ -9,8 +9,42 @@
 
             this.state = {
                 count: 0,
-                count2: 1
+                count2: 1, 
+                userInfo: {
+                    name: "Dummy Name",
+                    location: "Default Location"
+                }
             };
+        }
+
+        async componentDidMount() {
+
+            async function fetchData() {
+                const data = await fetch("https://api.github.com/users/d2j1");
+                const json = await data.json();
+
+                // console.log(json);
+              
+                return json;
+            }
+            const userInfo = await fetchData();
+                 this.setState({
+                    userInfo: userInfo
+                });
+            console.log("Component Did Mount");
+
+            this.timer = setInterval(() => {
+                console.log("Namaste React OP");
+            }, 1000);
+        }
+
+        componentDidUpdate(){
+            console.log("Component Did Update");
+        }
+
+        componentWillUnmount(){
+            console.log("Component Will Unmount");
+            clearInterval(this.timer);
         }
 
         render() {
@@ -22,7 +56,8 @@
                 <div className="user-card">
                     <h2>Name: {this.props.name}</h2>
                     <h2>Name: {name} {"Destructured"}</h2>
-                    <h3>Location: Delhi</h3>
+                    <h3> API DATA :- Name: {this.state.userInfo.name}</h3>
+                    <h3>API DATA :- Location: {this.state.userInfo.location}</h3>
                     <h4>Contact: 123-456-7890</h4>
                     <h4>Count: {this.state.count}</h4>
                     <button onClick={() => {
